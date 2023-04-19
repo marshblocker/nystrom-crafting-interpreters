8b65a4546771db317f188bb633b0d2a9607cd6db
marshblocker <gabrielkennethmarinas@gmail.com>
2023-04-19 16:18:25 +0800

    Move KEYWORDS map to token.rs

--- rlox/src/token.rs
+++ rlox/src/token.rs
@@ -1,3 +1,5 @@
+use phf::phf_map;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum TokenType {
     // Single-character tokens.
@@ -48,6 +50,25 @@ pub enum TokenType {
 
     EOF,
 }
+
+pub static KEYWORDS: phf::Map<&'static str, TokenType> = phf_map! {
+    "and" => TokenType::And,
+    "class" => TokenType::Class,
+    "else" => TokenType::Else,
+    "false" => TokenType::False,
+    "for" => TokenType::For,
+    "fun" => TokenType::Fun,
+    "if" => TokenType::If,
+    "nil" => TokenType::Nil,
+    "or" => TokenType::Or,
+    "print" => TokenType::Print,
+    "return" => TokenType::Return,
+    "super" => TokenType::Super,
+    "this" => TokenType::This,
+    "true" => TokenType::True,
+    "var" => TokenType::Var,
+    "while" => TokenType::While,
+};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Token {
